07aeb7823f7e491397cb79a78ff756666789c005
Rudolfs Klavins <klavins.rudolfs94@gmail.com>
2020-06-07 21:04:19 +0300

    main and buttons done

--- fizz1.js
+++ fizz1.js
@@ -1,37 +1,44 @@
-console.log("Started Clearing js");
+function main() {
+    //Add Listeners to each button
+    const innerCont = document.getElementById("id-inner-cont");
+    const minCountEl = document.getElementById("min-count");
+    const maxCountEl = document.getElementById("max-count");
 
-//Add Listeners to each button
+    let minCount = 1; //we use to cache minCountEl.value
+    let maxCount = 100; //we use to cache maxCountEl.value
 
-function addFizElements() {
-    console.log("Adding Fizz Elements");
+    function addFizElements() {
+        console.log("Adding Fizz Elements");
+    }
+
+    function addBuzElements() {
+        console.log("Adding Buzz Elements");
+    }
+
+    function addFizBuzElements() {
+        console.log("Adding Default Elements");
+    }
+
+    function deleteElements() {
+        console.log("Clearing Elements");
+    }
+
+    function addEventHandlers() {
+        console.log("adding Handlers");
+        const addFizbtn = document.getElementById("btn-id-add-fiz");
+        addFizbtn.onclick = addFizElements;
+        const addBuzBtn = document.getElementById("btn-id-add-buz");
+        addBuzBtn.onclick = addBuzElements;
+        const addFizBuzBtn = document.getElementById("btn-id-add-fiz-buz");
+        addFizBuzBtn.onclick = addFizBuzElements;
+        const addDefaultBtn = document.getElementById("btn-id-add-default");
+        addDefaultBtn.onclick = addFizBuzElements;
+        const clearBtn = document.getElementById("btn-id-clear");
+        clearBtn.onclick = deleteElements;
+        //add event handlers here
+        //you will need to find the elements
+    }
+
+    addEventHandlers();
 }
-
-function addBuzElements() {
-    console.log("Adding Buzz Elements");
-}
-
-function addFizBuzElements() {
-    console.log("Adding Default Elements");
-}
-
-function deleteElements() {
-    console.log("Clearing Elements");
-}
-
-function addEventHandlers() {
-    console.log("adding Handlers");
-    const addFizbtn = document.getElementById("btn-id-add-fiz");
-    addFizbtn.onclick = addFizElements;
-    const addBuzBtn = document.getElementById("btn-id-add-buz");
-    addBuzBtn.onclick = addBuzElements;
-    const addFizBuzBtn = document.getElementById("btn-id-add-fiz-buz");
-    addFizBuzBtn.onclick = addFizBuzElements;
-    const addDefaultBtn = document.getElementById("btn-id-add-default");
-    addDefaultBtn.onclick = addFizBuzElements;
-    const clearBtn = document.getElementById("btn-id-clear");
-    clearBtn.onclick = deleteElements;
-    //add event handlers here
-    //you will need to find the elements
-}
-
-addEventHandlers();
+main();
